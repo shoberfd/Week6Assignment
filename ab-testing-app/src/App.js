d8e98bc07e07b61dcb0c38f1a436914ce05a64c1
Shober-Fernback <shoberfd@csp.edu>
2024-12-08 09:36:29 -0600

    Added needed .js files and created App.js

--- ab-testing-app/src/App.js
+++ ab-testing-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // router 
+import IntroPage from './IntroPage';
+import LoginPage from './LoginPage';
+import HomePage from './HomePage'; 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( // sequence users go through: Intro -> Login -> Home (either A or B)
+    <Router>
+      <Routes> 
+        <Route path="/" element={<IntroPage />} /> 
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} /> 
+      </Routes>
+    </Router>
   );
 }
 
